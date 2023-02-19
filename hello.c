c06cea38565333bc7fae52e966aacb4914577194
Milena <milena_bakalova@abv.bg>
2023-02-19 17:59:20 +0200

    new printf

--- hello.c
+++ hello.c
@@ -6,6 +6,13 @@ int main()
 	printf("Hello \r\n");
 	printf("Hello 300000\r\n");
 
+    printf("Hello \r\n");
+	printf("Hello \r\n");
+	printf("Hello 300000\r\n");
+
+        printf("Hello \r\n");
+	printf("Hello \r\n");
+	printf("Hello 300000\r\n");
 
     return 0;
 }
